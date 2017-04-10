006325dcb98daa012cd199039a59c53897f29dc8
winagensotto <wina.gen.sotto@gmail.com>
2017-04-10 10:45:12 +0800

    uhhhmbot

--- app/src/main/java/com/dearnikki/dearnikki/RegisterRequest.java
+++ app/src/main/java/com/dearnikki/dearnikki/RegisterRequest.java
@@ -19,8 +19,8 @@ public class RegisterRequest extends StringRequest {
         params = new HashMap<>();
         params.put("name", name);
         params.put("username", username);
-        params.put("password", password);
         params.put("age", age + "");
+        params.put("password", password);
     }
 
     @Override
--- app/src/main/java/com/dearnikki/dearnikki/UserAreaActivity.java
+++ app/src/main/java/com/dearnikki/dearnikki/UserAreaActivity.java
@@ -1,5 +1,6 @@
 package com.dearnikki.dearnikki;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -15,5 +16,16 @@ public class UserAreaActivity extends AppCompatActivity {
         final EditText etUsername = (EditText) findViewById(R.id.etUsername);
         final EditText etAge = (EditText) findViewById(R.id.etAge);
         final TextView welcomeMessage = (TextView) findViewById(R.id.tvWelcomeMsg);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String username = intent.getStringExtra("username");
+        int age = intent.getIntExtra("age", -1);
+
+        String message = name + ", welcome to your user area!";
+        welcomeMessage.setText(message);
+        etUsername.setText(username);
+        etAge.setText(age + "");
+
     }
 }
